8395e5dc6c1627406b96cc332136b8e48be569ed
Pierre Krieger <pierre.krieger1708@gmail.com>
2025-03-13 13:10:35 +0100

    Reapply changes

--- src/astcenc_entry_cbe.rs
+++ src/astcenc_entry_cbe.rs
@@ -1890,13 +1890,9 @@ unsafe extern "C" fn llvm_OC_umul_OC_with_OC_overflow_OC_i64(
         field0: 0,
         field1: 0,
     };
-    r.field1 = LLVMMul_uov(
-        (8 as libc::c_int as libc::c_ulong)
-            .wrapping_mul(::core::mem::size_of::<uint64_t>() as libc::c_ulong),
-        &mut a,
-        &mut b,
-        &mut r.field0,
-    ) as bool_0;
+    let (out, carry) = a.overflowing_mul(b);
+    r.field0 = out;
+    r.field1 = carry as bool_0;
     return r;
 }
 #[no_mangle]
@@ -21455,7 +21451,7 @@ pub unsafe extern "C" fn _ZNSt11unique_lockISt5mutexEC2ERS0_(
     _5431 = _5429;
     _5432 = _5430;
     _5433 = _ZSt11__addressofISt5mutexEPT_RS1_(_5432);
-    let ref mut fresh21 = *(&mut (*(_5431 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field0
+    let ref mut fresh21 = *(&raw mut (*(_5431 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field0
         as *mut *mut libc::c_void);
     *fresh21 = _5433;
     *(&mut (*(_5431 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field1 as *mut uint8_t) =
@@ -21530,7 +21526,7 @@ pub unsafe extern "C" fn _ZNSt11unique_lockISt5mutexE4lockEv(mut _5456: *mut lib
     let mut _5461: *mut libc::c_void = 0 as *mut libc::c_void;
     _5457 = _5456;
     _5458 = _5457;
-    _5459 = *(&mut (*(_5458 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field0
+    _5459 = *(&raw mut (*(_5458 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field0
         as *mut *mut libc::c_void);
     if !_5459.is_null() {
         _5460 = *(&mut (*(_5458 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field1
@@ -21538,7 +21534,7 @@ pub unsafe extern "C" fn _ZNSt11unique_lockISt5mutexE4lockEv(mut _5456: *mut lib
         if _5460 as libc::c_uint & 1 as libc::c_uint != 0 {
             _ZSt20__throw_system_errori(35 as libc::c_int as uint32_t);
         } else {
-            _5461 = *(&mut (*(_5458 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field0
+            _5461 = *(&raw mut (*(_5458 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field0
                 as *mut *mut libc::c_void);
             _ZNSt5mutex4lockEv(_5461);
             *(&mut (*(_5458 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field1
@@ -21655,10 +21651,10 @@ pub unsafe extern "C" fn _ZNSt11unique_lockISt5mutexE6unlockEv(mut _5503: *mut l
     _5506 =
         *(&mut (*(_5505 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field1 as *mut uint8_t);
     if _5506 as libc::c_uint & 1 as libc::c_uint != 0 {
-        _5507 = *(&mut (*(_5505 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field0
+        _5507 = *(&raw mut (*(_5505 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field0
             as *mut *mut libc::c_void);
         if !_5507.is_null() {
-            _5508 = *(&mut (*(_5505 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field0
+            _5508 = *(&raw mut (*(_5505 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field0
                 as *mut *mut libc::c_void);
             _ZNSt5mutex6unlockEv(_5508);
             *(&mut (*(_5505 as *mut l_struct_class_OC_std_KD__KD_unique_lock)).field1
