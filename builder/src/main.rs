836ab36a93bbf414f1a8e0a66c60a81a09cb35d4
Pierre Krieger <pierre.krieger1708@gmail.com>
2025-03-22 16:37:22 +0100

    WIP

--- builder/src/main.rs
+++ builder/src/main.rs
@@ -66,6 +66,50 @@ fn main() {
     #RUN cd src && for f in *.rs; do sed -i 's/pub struct __tmp_rename_array_\([_a-zA-Z0-9]*\) {/pub struct l_array_\1 {/g' $f; done
     */
 
+    // Add some definitions at the end of `lib.rs`.
+    let lib_rs_padding: Vec<syn::ItemFn> = vec![
+        syn::parse_quote! {  unsafe fn memcpy(d: *mut core::ffi::c_void, s: *mut core::ffi::c_void, c: u64) -> *mut core::ffi::c_void { core::ptr::copy_nonoverlapping::<u8>(s.cast_const().cast(), d.cast(), c as usize); d } },
+        syn::parse_quote! {  unsafe fn memset(d: *mut core::ffi::c_void, ch: core::ffi::c_int, c: u64) -> *mut core::ffi::c_void { assert!(ch <= 255); core::ptr::write_bytes::<u8>(d.cast(), ch as u8, c as usize); d } },
+        syn::parse_quote! {  fn abs(v: u32) -> u32 { v } },
+        syn::parse_quote! {  fn cosf(v: f32) -> f32 { v.cos() } },
+        syn::parse_quote! {  fn sinf(v: f32) -> f32 { v.sin() } },
+        syn::parse_quote! {  fn fabsf(v: f32) -> f32 { v.abs() } },
+        syn::parse_quote! {  fn roundf(v: f32) -> f32 { v.round() } },
+        syn::parse_quote! {  fn sqrtf(v: f32) -> f32 { v.sqrt() } },
+        syn::parse_quote! {  fn logf(v: f32) -> f32 { v.ln() } },
+        syn::parse_quote! {  fn fegetround() -> core::ffi::c_uint { 0 } },
+        syn::parse_quote! {  fn __assert_fail(_assertion: *mut core::ffi::c_void, _file: *mut core::ffi::c_void, _line: core::ffi::c_uint, _function: *mut core::ffi::c_void) -> ! { panic!() } },
+        syn::parse_quote! {  fn LLVMMul_uov(_: core::ffi::c_ulong, a: &mut u64, b: &mut u64, out: &mut u64) -> u8 { let (res, carry) = (*a).overflowing_mul(*b); *out = res; carry as u8 } },
+        syn::parse_quote! {  unsafe fn posix_memalign(memptr: *mut core::ffi::c_void, align: u64, size: u64) -> core::ffi::c_int { libc::posix_memalign(memptr as *mut _, align as usize, size as usize) } },
+        syn::parse_quote! {  unsafe fn free(ptr: *mut core::ffi::c_void) { libc::free(ptr) } },
+        syn::parse_quote! {  unsafe fn _Znwm(size: u64) -> *mut core::ffi::c_void { libc::malloc(size as libc::size_t) } },
+        syn::parse_quote! {  unsafe fn _Znam(size: u64) -> *mut core::ffi::c_void { libc::malloc(size as libc::size_t) } },
+        syn::parse_quote! {  unsafe fn _ZdaPv(ptr: *mut core::ffi::c_void) { libc::free(ptr) } },
+        syn::parse_quote! {  unsafe fn _ZdlPvm(ptr: *mut core::ffi::c_void, _: u64) { libc::free(ptr) } },
+        syn::parse_quote! {  unsafe fn _ZSt25__throw_bad_function_callv() -> ! { panic!() } },
+        syn::parse_quote! {  unsafe fn _ZSt20__throw_system_errori<T>(_: T) -> ! { panic!() } },
+        syn::parse_quote! {
+            // __cxa_begin_catch is only ever used right before a call to terminate() },
+            unsafe fn __cxa_begin_catch(_: *mut core::ffi::c_void) -> *mut core::ffi::c_void { core::ptr::null_mut() }
+        },
+        syn::parse_quote! {  unsafe fn _ZSt9terminatev() -> ! { panic!() } },
+        syn::parse_quote! {  unsafe fn pthread_mutex_lock(mutex: *mut core::ffi::c_void) -> core::ffi::c_uint { libc::pthread_mutex_lock(mutex as *mut _) as core::ffi::c_uint } },
+        syn::parse_quote! {  unsafe fn pthread_mutex_unlock(mutex: *mut core::ffi::c_void) -> core::ffi::c_uint { libc::pthread_mutex_unlock(mutex as *mut _) as core::ffi::c_uint } },
+    ];
+    std::io::Write::write_all(
+        &mut fs::File::options()
+            .append(true)
+            .open(target_rust_dir.join("lib.rs"))
+            .unwrap(),
+        lib_rs_padding
+            .iter()
+            .map(|q| quote::ToTokens::to_token_stream(q).to_string())
+            .collect::<Vec<_>>()
+            .join("")
+            .as_bytes(),
+    )
+    .unwrap();
+
     // In practice, the Rust code only ever works through raw pointers
     // We replace `&mut *some_val` with `&raw mut *some_val` (and same with `const`).
     // This has the effect of removing compilation errors due to `as *mut other_type` converting from
@@ -184,6 +228,17 @@ fn main() {
                     new_path.push(syn::PathSegment::from(
                         syn::parse_str::<syn::Ident>(module.to_str().unwrap()).unwrap(),
                     ));
+                    new_path.push(path.segments.iter().next().unwrap().clone());
+                    path.segments = new_path;
+                } else if lib_rs_padding
+                    .iter()
+                    .any(|f| f.sig.ident == path.segments.iter().next().unwrap().ident)
+                {
+                    // If referring to a function added to lib.rs, fix the path as well.
+                    let mut new_path = syn::punctuated::Punctuated::new();
+                    new_path.push(syn::PathSegment::from(syn::Ident::from(
+                        <syn::Token!(crate)>::default(),
+                    )));
                     new_path.push(path.segments.iter().next().unwrap().clone());
                     path.segments = new_path;
                 }
@@ -456,35 +511,4 @@ fn main() {
         let content = quote::ToTokens::into_token_stream(target_content).to_string();
         fs::write(target_path, &content).unwrap();
     }
-
-    std::io::Write::write_all(
-        &mut fs::File::options().append(true).open(target_rust_dir.join("lib.rs")).unwrap(),
-        quote::quote! {
-            unsafe fn memcpy(d: *mut core::ffi::c_void, s: *mut core::ffi::c_void, c: u64) -> *mut core::ffi::c_void { core::ptr::copy_nonoverlapping::<u8>(s.cast_const().cast(), d.cast(), c as usize); d }
-            unsafe fn memset(d: *mut core::ffi::c_void, ch: core::ffi::c_int, c: u64) -> *mut core::ffi::c_void { assert!(ch <= 255); core::ptr::write_bytes::<u8>(d.cast(), ch as u8, c as usize); d }
-            fn abs(v: u32) -> u32 { v }
-            fn cosf(v: f32) -> f32 { v.cos() }
-            fn sinf(v: f32) -> f32 { v.sin() }
-            fn fabsf(v: f32) -> f32 { v.abs() }
-            fn roundf(v: f32) -> f32 { v.round() }
-            fn sqrtf(v: f32) -> f32 { v.sqrt() }
-            fn logf(v: f32) -> f32 { v.ln() }
-            fn fegetround() -> core::ffi::c_uint { 0 }
-            fn __assert_fail(_assertion: *mut core::ffi::c_void, _file: *mut core::ffi::c_void, _line: core::ffi::c_uint, _function: *mut core::ffi::c_void) -> ! { panic!() }
-            fn LLVMMul_uov(_: core::ffi::c_ulong, a: &mut u64, b: &mut u64, out: &mut u64) -> u8 { let (res, carry) = (*a).overflowing_mul(*b); *out = res; carry as u8 }
-            unsafe fn posix_memalign(memptr: *mut core::ffi::c_void, align: u64, size: u64) -> core::ffi::c_int { libc::posix_memalign(memptr as *mut _, align as usize, size as usize) }
-            use libc::free;
-            unsafe fn _Znwm(size: u64) -> *mut core::ffi::c_void { libc::malloc(size as libc::size_t) }
-            unsafe fn _Znam(size: u64) -> *mut core::ffi::c_void { libc::malloc(size as libc::size_t) }
-            unsafe fn _ZdaPv(ptr: *mut core::ffi::c_void) { libc::free(ptr) }
-            unsafe fn _ZdlPvm(ptr: *mut core::ffi::c_void, _: u64) { libc::free(ptr) }
-            unsafe fn _ZSt25__throw_bad_function_callv() -> ! { panic!() }
-            unsafe fn _ZSt20__throw_system_errori<T>(_: T) -> ! { panic!() }
-            // __cxa_begin_catch is only ever used right before a call to terminate()
-            unsafe fn __cxa_begin_catch(_: *mut core::ffi::c_void) -> *mut core::ffi::c_void { core::ptr::null_mut() }
-            unsafe fn _ZSt9terminatev() -> ! { panic!() }
-            unsafe fn pthread_mutex_lock(mutex: *mut core::ffi::c_void) -> core::ffi::c_uint { libc::pthread_mutex_lock(mutex as *mut _) as core::ffi::c_uint }
-            unsafe fn pthread_mutex_unlock(mutex: *mut core::ffi::c_void) -> core::ffi::c_uint { libc::pthread_mutex_unlock(mutex as *mut _) as core::ffi::c_uint }
-        }.to_string().as_bytes()
-    ).unwrap();
 }
